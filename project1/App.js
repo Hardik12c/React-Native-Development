6d693ac57c1250ba47b296c018247b2ab9a82732
Hardik <hardik12c@gmail.com>
2023-01-05 06:54:33 +0530

    flexbox understanding

--- project1/App.js
+++ project1/App.js
@@ -1,17 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView,TextInput,Button} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Button,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.flexcontainer}>
-        <TextInput style={styles.textinputstyle} placeholder='Enter text here...'/>
-        <Button title='ADD'/>
+      <View
+        style={{
+          backgroundColor: "red",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems:'center',
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View>
-        <Text>
-            your Goals are here
-        </Text>
+      <View style={{
+          backgroundColor: "green",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems:'center',
+        }}>
+        <Text>2</Text>
+      </View>
+      <View style={{
+          backgroundColor: "blue",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems:'center',
+        }}>
+        <Text>3</Text>
       </View>
     </SafeAreaView>
   );
@@ -20,15 +47,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding:50,
-  },
-  flexcontainer:{
-    flexDirection:'row',
-  },
-  textinputstyle:{
-    borderWidth:1,
-    borderColor:'blue',
-    width:'80%',
-    backgroundColor:'#cccccc',
+    padding: 50,
+    flexDirection:'column',
+    alignItems:'flex-end',
   }
+  // flexcontainer: {
+  //   flexDirection: "row",
+  // },
+  // textinputstyle: {
+  //   borderWidth: 1,
+  //   borderColor: "blue",
+  //   width: "80%",
+  //   backgroundColor: "#cccccc",
+  // },
 });
