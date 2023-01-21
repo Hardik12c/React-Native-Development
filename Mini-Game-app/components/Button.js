82c75afa1af763a6fcb611a5b5096c124d008cb1
Hardik <hardik12c@gmail.com>
2023-01-22 03:11:34 +0530

    button improved and input style changed

--- Mini-Game-app/components/Button.js
+++ Mini-Game-app/components/Button.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Button(props) {
+  const handlepress = () => {
+    console.log("pressed");
+  };
   return (
-    <View>
-        <Text>{props.children}</Text>
+    <View style={styles.buttoncontainer}>
+      <Pressable style={styles.innercontainer} android_ripple={{color:'#600432'}} onPress={handlepress}>
+        <Text style={styles.buttontext}>{props.children}</Text>
+      </Pressable>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  innercontainer: {
+    backgroundColor: "#72063c",
+    borderRadius: 28,
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+    margin: 4,
+  },
+  buttontext: {
+    color:'white',
+    textAlign: "center",
+  },
+});
--- Mini-Game-app/screens/Gamestart.js
+++ Mini-Game-app/screens/Gamestart.js
@@ -1,28 +1,46 @@
-import React from 'react'
-import { StyleSheet, TextInput, View } from 'react-native'
-import Button from '../components/Button'
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import Button from "../components/Button";
 
 export default function Gamestart() {
   return (
     <View style={styles.inputcontainer}>
-        <TextInput/>
-        <Button>Reset</Button>
-        <Button>Confirm</Button>
+      <TextInput
+        style={styles.numberinput}
+        maxLength={2}
+        autoCapitalize="none"
+        keyboardType="number-pad"
+        autoCorrect={false}
+      />
+      <Button>Reset</Button>
+      <Button>Confirm</Button>
     </View>
-  )
+  );
 }
 
-const styles=StyleSheet.create({
-    inputcontainer:{
-        marginHorizontal:24,
-        borderRadius:8,
-        marginTop:100,
-        padding:6,
-        backgroundColor:'#72063c',
-        elevation:10,
-        shadowColor:'green',
-        shadowOffset:{width:20,height:50},
-        shadowRadius:25,
-        shadowOpacity:1,
-    }
-})
+const styles = StyleSheet.create({
+  inputcontainer: {
+    marginHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 100,
+    padding: 6,
+    backgroundColor: "#4e0329",
+    elevation: 10,
+    shadowColor: "#000000",
+    shadowOffset: { width: 20, height: 50 },
+    shadowRadius: 25,
+    shadowOpacity: 1,
+  },
+  numberinput: {
+    height: 50,
+    width: 50,
+    fontWeight: "bold",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    fontWeight: "bold",
+    fontSize: 32,
+    marginVertical: 8,
+    textAlign: "center",
+  },
+});
