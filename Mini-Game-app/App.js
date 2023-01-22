05d8823c9cfea3667c3856193a883837e9efc411
Hardik <hardik12c@gmail.com>
2023-01-23 01:59:49 +0530

    main ui screen completed

--- Mini-Game-app/App.js
+++ Mini-Game-app/App.js
@@ -1,12 +1,26 @@
-import { StyleSheet} from 'react-native';
-import Gamestart from './screens/Gamestart';
-
+import { ImageBackground, StyleSheet } from "react-native";
+import Gamestart from "./screens/Gamestart";
+import { LinearGradient } from "expo-linear-gradient";
 export default function App() {
   return (
-    <Gamestart/>
+    <LinearGradient style={styles.root} colors={["#4e0329", "#edc43f"]}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        style={styles.root}
+        resizeMode={'cover'}
+        imageStyle={styles.imageStyle}
+      >
+        <Gamestart />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  root: {
+    flex: 1,
+  },
+  imageStyle:{
+    opacity:0.12,
+  }
 });
--- Mini-Game-app/screens/Gamestart.js
+++ Mini-Game-app/screens/Gamestart.js
@@ -12,19 +12,27 @@ export default function Gamestart() {
         keyboardType="number-pad"
         autoCorrect={false}
       />
-      <Button>Reset</Button>
-      <Button>Confirm</Button>
+      <View style={styles.buttoncontainer}>
+        <View style={{flex:1}}>
+          <Button>Reset</Button>
+        </View>
+        <View style={{flex:1}}>
+          <Button>Confirm</Button>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputcontainer: {
+    alignItems: "center",
+    justifyContent: "center",
     marginHorizontal: 24,
     borderRadius: 8,
     marginTop: 100,
     padding: 6,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b0421",
     elevation: 10,
     shadowColor: "#000000",
     shadowOffset: { width: 20, height: 50 },
@@ -42,5 +50,8 @@ const styles = StyleSheet.create({
     fontSize: 32,
     marginVertical: 8,
     textAlign: "center",
+  },
+  buttoncontainer: {
+    flexDirection: "row",
   },
 });
